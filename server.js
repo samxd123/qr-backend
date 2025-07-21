9f4e6ee72e519dba9662bb235d0172f5fcb1daec
samxd123 <leasyto@gmail.com>
2025-07-22 00:31:00 +0530

    Clean backend for deployment

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 });
 
 
-mongoose.connect('process.env.MONGO_URI', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
