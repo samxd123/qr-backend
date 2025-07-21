83199002d311e34fbbb144d7145b1a650fdbccd4
samxd123 <leasyto@gmail.com>
2025-07-22 00:11:43 +0530

    Clean backend for deployment

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/Product');
+const Product = require('../models/product');
 const QRCode = require('qrcode');
 const fs = require('fs');
 const path = require('path');
