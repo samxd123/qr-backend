8dbf293a97fad575f2964037dfd2e9a7a76663ac
samxd123 <leasyto@gmail.com>
2025-07-22 00:17:50 +0530

    Clean backend for deployment

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 });
 
 
-mongoose.connect('mongodb+srv://leasyto:z2TeL479ykSSBdxc@samclus.yytjfbu.mongodb.net/?retryWrites=true&w=majority&appName=SamClus', {
+mongoose.connect('process.env.MONGO_URI', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
